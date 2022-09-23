f7ce523f39901ab465f28894648c05cf7f60a1a6
03bennej <jjrbenn@gmail.com>
2022-09-23 14:52:00 -0400

    metrics added

--- ei.py
+++ ei.py
@@ -165,8 +165,6 @@ class EnsembleIntegration:
         self.meta_predictions = pd.DataFrame.from_dict(meta_predictions)
         self.meta_summary = metric_threshold_dataframes(self.meta_predictions)
 
-        print(self.meta_summary["metrics"])
-
         return self
 
     @ignore_warnings(category=ConvergenceWarning)
@@ -194,8 +192,6 @@ class EnsembleIntegration:
         meta_test_averaged_samples["labels"] = labels
 
         self.base_summary = metric_threshold_dataframes(meta_test_averaged_samples)
-
-        print("\n" + self.base_summary["metrics"])
 
         return self
 
