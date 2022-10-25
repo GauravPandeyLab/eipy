892cae1a423e4d9ff46863dbd77ffdfeaa014f6e
03bennej <jjrbenn@gmail.com>
2022-10-25 10:54:58 -0400

    minor changes

--- utils.py
+++ utils.py
@@ -13,9 +13,9 @@ class TFWrapper:
         self.compile_kwargs = compile_kwargs
         self.fit_kwargs = fit_kwargs
 
-        self.tf_model.compile(**self.compile_kwargs)
 
     def fit(self, X, y):
+        self.tf_model.compile(**self.compile_kwargs)
         self.tf_model.set_weights(self.initial_weights)  # re-initialises weights for multiple .fit calls
         self.new_weights = self.tf_model.get_weights()
         self.tf_model.fit(X, y, verbose=0, **self.fit_kwargs)
