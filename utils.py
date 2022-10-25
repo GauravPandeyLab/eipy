9faeb9a1d159cbac53bc916ad21ae3e26e13fcfe
03bennej <jjrbenn@gmail.com>
2022-10-25 17:52:33 -0400

    minor changes

--- utils.py
+++ utils.py
@@ -4,26 +4,25 @@ import random
 from sklearn.metrics import roc_auc_score, precision_recall_curve, matthews_corrcoef
 from imblearn.under_sampling import RandomUnderSampler
 from imblearn.over_sampling import RandomOverSampler
-from tensorflow.keras.models import clone_model
+from tensorflow.keras.backend import clear_session
 
 class TFWrapper:
-    def __init__(self, tf_model, compile_kwargs, fit_kwargs):
-        self.tf_model = clone_model(tf_model)
-        self.initial_weights = self.tf_model.get_weights()
+    def __init__(self, tf_fun, compile_kwargs, fit_kwargs):
+        self.tf_fun = tf_fun
+        # self.initial_weights = self.tf_model.get_weights()
         self.compile_kwargs = compile_kwargs
         self.fit_kwargs = fit_kwargs
 
         # self.tf_model.compile(**self.compile_kwargs)
 
     def fit(self, X, y):
-        self.model_new = clone_model(self.tf_model)
-        self.model_new.compile(**self.compile_kwargs)
-        self.model_new.set_weights(self.initial_weights)  # re-initialises weights for multiple .fit calls
-        self.model_new.compile(**self.compile_kwargs)
-        self.model_new.fit(X, y, verbose=0, **self.fit_kwargs)
+        self.model = self.tf_fun()
+        self.model.compile(**self.compile_kwargs)
+        # self.model.set_weights(self.initial_weights)  # re-initialises weights for multiple .fit calls
+        self.model.fit(X, y, verbose=0, **self.fit_kwargs)
 
     def predict_proba(self, X):
-        return np.squeeze(self.model_new.predict(X))
+        return np.squeeze(self.model.predict(X))
 
 
 def score_threshold_vectors(df, labels):
