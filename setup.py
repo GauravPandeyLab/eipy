4ed84962b2cfbae01e069b8b8b3032a74a66497f
03bennej <jjrbenn@gmail.com>
2023-04-14 19:34:15 +0000

    package update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='ei-python', 
+      version='0.0.1', 
+      description='Ensemble Integration (EI)',
+      url='https://github.com/03bennej/ei-python.git',
+      author='Jamie Bennett, Richard Li',
+      license='CC by 4.0',
+      packages=find_packages(),
+      )
